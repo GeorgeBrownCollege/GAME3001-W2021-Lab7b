a2cea1601369e279f3717534532970eaafee6a40
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-03-16 16:22:57 -0400

    converted ship to Agent

--- src/ship.h
+++ src/ship.h
@@ -2,11 +2,12 @@
 #ifndef __SHIP__
 #define __SHIP__
 
-#include "DisplayObject.h"
 #include "TextureManager.h"
 #include <glm/vec4.hpp>
 
-class Ship final : public DisplayObject
+#include "Agent.h"
+
+class Ship final : public Agent
 {
 public:
 	Ship();
@@ -25,40 +26,18 @@ public:
 	void move();
 
 	// getters
-	glm::vec2 getTargetPosition() const;
-	glm::vec2 getCurrentDirection() const;
 	float getMaxSpeed() const;
 	
-	float getLOSDistance() const;
-	bool hasLOS() const;
-	float getCurrentHeading() const;
-
 	// setters
-	void setTargetPosition(glm::vec2 newPosition);
-	void setCurrentDirection(glm::vec2 newDirection);
 	void setMaxSpeed(float newSpeed);
-
-	void setLOSDistance(float distance);
-	void setHasLOS(bool state);
-	void setCurrentHeading(float heading);
 
 private:
 	void m_checkBounds();
 	void m_reset();
 
-	// steering behaviours
 	float m_maxSpeed;
 	float m_turnRate;
 
-	void m_changeDirection();
-	float m_currentHeading;
-	glm::vec2 m_currentDirection;
-	glm::vec2 m_targetPosition;
-
-	// LOS
-	float m_LOSDistance;
-	bool m_hasLOS;
-	glm::vec4 m_LOSColour;
 };
 
 
